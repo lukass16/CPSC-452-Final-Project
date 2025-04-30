4e720cd2f049c28f2f7bcd001e3fc30ac5df85bb
lukass16 <lukasskellijs@gmail.com>
2025-04-30 14:18:24 -0400

    Comment and Fix Point Cloud Confusion

--- layers.py
+++ layers.py
@@ -1,16 +1,17 @@
+""" Imports """
 from functools import wraps
 
-import numpy as np
-
+# general imports
 import torch
 from torch import nn, einsum
 import torch.nn.functional as F
+import numpy as np
+from einops import rearrange, repeat # tensor manipulation
+from torch_cluster import fps # further point sampling
+from timm.layers import DropPath
 
-from einops import rearrange, repeat
 
-from torch_cluster import fps
-from timm.models.layers import DropPath
-
+""" Utility functions"""
 def exists(val):
     return val is not None
 
@@ -30,29 +31,49 @@ def cache_fn(f):
         return cache
     return cached_fn
 
+
+""" Layers """
 class PreNorm(nn.Module):
+    '''
+    Pre-norm layer that applies layer normalization and wraps the layer fn.
+    
+    Args:
+        dim (int): Dimension of the input tensor.
+        fn (callable): Function to apply after normalization.
+        context_dim (int, optional): Dimension of the context tensor, if applicable.
+    '''
+    
     def __init__(self, dim, fn, context_dim = None):
         super().__init__()
         self.fn = fn
-        self.norm = nn.LayerNorm(dim)
-        self.norm_context = nn.LayerNorm(context_dim) if exists(context_dim) else None
+        self.norm = nn.LayerNorm(dim) # normalizes last dimension of input tensor with dimension dim - used to normalize embedded point feature vector
+        self.norm_context = nn.LayerNorm(context_dim) if exists(context_dim) else None # if there are context vectors, normalize them as well
 
     def forward(self, x, **kwargs):
         x = self.norm(x)
 
-        if exists(self.norm_context):
+        if exists(self.norm_context): # if passed a context_dim retrieve and normalize the context vectors as well 
             context = kwargs['context']
             normed_context = self.norm_context(context)
             kwargs.update(context = normed_context)
 
         return self.fn(x, **kwargs)
-    
+  
+  
 class GEGLU(nn.Module):
     def forward(self, x):
         x, gates = x.chunk(2, dim = -1)
         return x * F.gelu(gates)
 
 class FeedForward(nn.Module):
+    ''' Feed-forward layer with GEGLU activation 
+    
+        Args:
+            dim (int): Dimension of the input tensor.
+            mult (int, optional): Multiplier for the hidden dimension. Defaults to 4.
+            drop_path_rate (float, optional): Drop path rate for stochastic depth regularization. Defaults to 0.0.
+    '''
+    
     def __init__(self, dim, mult = 4, drop_path_rate = 0.0):
         super().__init__()
         self.net = nn.Sequential(
@@ -64,9 +85,21 @@ class FeedForward(nn.Module):
         self.drop_path = DropPath(drop_path_rate) if drop_path_rate > 0. else nn.Identity()
 
     def forward(self, x):
-        return self.drop_path(self.net(x))
+        return self.drop_path(self.net(x)) # returns tensor of the same shape as input x after applying the feed-forward network and drop path regularization
 
 class Attention(nn.Module):
+    '''
+    Multi-head attention layer.
+
+    Args:
+        query_dim (int): Dimension of the query vectors.
+        context_dim (int, optional): Dimension of the context vectors. Defaults to None, which uses query_dim - if not provided, does self-attention.
+        heads (int): Number of attention heads.
+        dim_head (int): Dimension of each attention head.
+        drop_path_rate (float): Drop path rate for stochastic depth regularization.
+    '''
+    
+    
     def __init__(self, query_dim, context_dim = None, heads = 8, dim_head = 64, drop_path_rate = 0.0):
         super().__init__()
         inner_dim = dim_head * heads
@@ -74,10 +107,10 @@ class Attention(nn.Module):
         self.scale = dim_head ** -0.5
         self.heads = heads
 
-        self.to_q = nn.Linear(query_dim, inner_dim, bias = False)
-        self.to_kv = nn.Linear(context_dim, inner_dim * 2, bias = False)
+        self.to_q = nn.Linear(query_dim, inner_dim, bias = False) # maps to query vectors of dimension inner_dim = dim_head * heads
+        self.to_kv = nn.Linear(context_dim, inner_dim * 2, bias = False) # maps to key and value vectors of dimension inner_dim = dim_head * heads
         self.to_out = nn.Linear(inner_dim, query_dim)
-
+        
         self.drop_path = DropPath(drop_path_rate) if drop_path_rate > 0. else nn.Identity()
 
     def forward(self, x, context = None, mask = None):
@@ -106,36 +139,48 @@ class Attention(nn.Module):
 
 
 class PointEmbed(nn.Module):
-    def __init__(self, hidden_dim=48, dim=128):
+    '''
+    Point embedding layer 
+    
+    Performs Fourier positional encoding on 3D points and then maps them into a higher-dimensional embedding space
+    '''
+    def __init__(self, embedding_dim=48, out_dim=128):
         super().__init__()
 
-        assert hidden_dim % 6 == 0
-
-        self.embedding_dim = hidden_dim
-        e = torch.pow(2, torch.arange(self.embedding_dim // 6)).float() * np.pi
+        assert embedding_dim % 6 == 0
+        self.embedding_dim = embedding_dim # positional embedding dimension (must be divisible by 6)
+        Fdim = self.embedding_dim // 6 # number of frequencies for each axis (x, y, z), 3 axis with sin and cos for each = 6
+        
+        # creates exponentially spaced frequencies for Fourier encoding
+        e = torch.pow(2, torch.arange(Fdim)).float() * np.pi # 1, 2, 4, 8, 16, ... (up to Fdim)
+        
+        # create the basis matrix for Fourier encoding with dimension (3, Fdim) - construct separate frequency bands for each axis
         e = torch.stack([
-            torch.cat([e, torch.zeros(self.embedding_dim // 6),
-                        torch.zeros(self.embedding_dim // 6)]),
-            torch.cat([torch.zeros(self.embedding_dim // 6), e,
-                        torch.zeros(self.embedding_dim // 6)]),
-            torch.cat([torch.zeros(self.embedding_dim // 6),
-                        torch.zeros(self.embedding_dim // 6), e]),
+            # x axis encodings
+            torch.cat([e, torch.zeros(Fdim),
+                        torch.zeros(Fdim)]), # e, 0, 0 (where each 0 also has size Fdim)
+            # y axis encodings
+            torch.cat([torch.zeros(Fdim), e,
+                        torch.zeros(Fdim)]), # 0, e, 0
+            # z axis encodings
+            torch.cat([torch.zeros(Fdim),
+                        torch.zeros(Fdim), e]), # 0, 0, e
         ])
-        self.register_buffer('basis', e)  # 3 x 16
+        self.register_buffer('basis', e)  # attaches the tensor e (under the name 'basis') to the module, so it is not a parameter but a buffer
 
-        self.mlp = nn.Linear(self.embedding_dim+3, dim)
+        self.mlp = nn.Linear(self.embedding_dim+3, out_dim) 
 
     @staticmethod
     def embed(input, basis):
         projections = torch.einsum(
-            'bnd,de->bne', input, basis)
+            'bnd,de->bne', input, basis) # input: B x N x 3, basis: 3 x Fdim -
         embeddings = torch.cat([projections.sin(), projections.cos()], dim=2)
         return embeddings
     
     def forward(self, input):
         # input: B x N x 3
         embed = self.mlp(torch.cat([self.embed(input, self.basis), input], dim=2)) # B x N x C
-        return embed
+        return embed # outputs B x N x dim - point embeddings for each point with dimension out_dim
 
 
 class DiagonalGaussianDistribution(object):
@@ -178,17 +223,18 @@ class DiagonalGaussianDistribution(object):
     def mode(self):
         return self.mean
 
+
 class KLAutoEncoder(nn.Module):
     def __init__(
         self,
         *,
-        depth=24,
-        dim=512,
+        depth=24, # depth of self-attention+feedforward blocks in decoder
+        dim=512, # dimension of point embeddings (also referred to as out_dim) (each point is represented by a vector of this dimension)
         queries_dim = 512,
         output_dim = 1,
         num_inputs = 2048,
         num_latents = 512,
-        latent_dim = 64,
+        latent_dim = 64, # smaller latent dimension for the latent vector set
         heads = 8,
         dim_head = 64,
         weight_tie_layers = False,
@@ -196,18 +242,20 @@ class KLAutoEncoder(nn.Module):
     ):
         super().__init__()
 
-        self.depth = depth
+        self.depth = depth # number of latent self-attention+feedforward blocks in decoder
 
-        self.num_inputs = num_inputs
-        self.num_latents = num_latents
+        self.num_inputs = num_inputs # number of input points
+        self.num_latents = num_latents # number of latent points - number of points in fps
 
+        """ Encoder Layers """
         self.cross_attend_blocks = nn.ModuleList([
-            PreNorm(dim, Attention(dim, dim, heads = 1, dim_head = dim), context_dim = dim),
+            PreNorm(dim, Attention(dim, dim, heads = 1, dim_head = dim), context_dim = dim), # takes input x (B, N, feature_dim), normalizes each feature (also for context), and applies attention 
             PreNorm(dim, FeedForward(dim))
         ])
 
-        self.point_embed = PointEmbed(dim=dim)
-
+        self.point_embed = PointEmbed(out_dim=dim)
+        
+        """ Decoder Layers """
         get_latent_attn = lambda: PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, drop_path_rate=0.1))
         get_latent_ff = lambda: PreNorm(dim, FeedForward(dim, drop_path_rate=0.1))
         get_latent_attn, get_latent_ff = map(cache_fn, (get_latent_attn, get_latent_ff))
@@ -228,66 +276,75 @@ class KLAutoEncoder(nn.Module):
 
         self.proj = nn.Linear(latent_dim, dim)
 
-        self.mean_fc = nn.Linear(dim, latent_dim)
+        self.mean_fc = nn.Linear(dim, latent_dim) #(we learn how to project each latent vector in the vector set to a latent mean vector of dimension latent_dim)
         self.logvar_fc = nn.Linear(dim, latent_dim)
 
     def encode(self, pc):
-        # pc: B x N x 3
+        # pc: B x N x 3 = point cloud with B batches, N points, and 3 coordinates (x, y, z)
         B, N, D = pc.shape
         assert N == self.num_inputs
         
-        ###### fps
-        flattened = pc.view(B*N, D)
+        """ Further point sampling """
+        all_points = pc.view(B*N, D) # flattens to one long vector of points
 
-        batch = torch.arange(B).to(pc.device)
-        batch = torch.repeat_interleave(batch, N)
+        batch = torch.arange(B).to(pc.device) # create batch indices/identifiers
+        batch = torch.repeat_interleave(batch, N) # repeat each batch index N times - "batch" now labels each point in the all_points vector with its corresponding batch
 
-        pos = flattened
+        ratio = 1.0 * self.num_latents / self.num_inputs # downsample ratio
 
-        ratio = 1.0 * self.num_latents / self.num_inputs
+        idx = fps(all_points, batch, ratio=ratio) # fps in batch mode, returns indices of sampled points
 
-        idx = fps(pos, batch, ratio=ratio)
+        sampled_pc = all_points[idx]
+        sampled_pc = sampled_pc.view(B, -1, 3) # go back to batches (B, N', 3) where N' is the number of sampled points
+        """ """
 
-        sampled_pc = pos[idx]
-        sampled_pc = sampled_pc.view(B, -1, 3)
-        ######
 
-        sampled_pc_embeddings = self.point_embed(sampled_pc)
-
-        pc_embeddings = self.point_embed(pc)
-
+        """ Point embedding """
+        sampled_pc_embeddings = self.point_embed(sampled_pc) # embed (B x N') sampled points (B, N', 3) to (B, N', out_dim)
+        pc_embeddings = self.point_embed(pc) # embed (B x N) original points (B, N, 3) to (B, N, out_dim)
+        """ """
+        
+        
+        """ Cross-attend from sampled points to original points + feedforward """
         cross_attn, cross_ff = self.cross_attend_blocks
+        x = cross_attn(sampled_pc_embeddings, context = pc_embeddings, mask = None) + sampled_pc_embeddings # cross attention + residual connection
+        x = cross_ff(x) + x # feedforward + residual connection (B, N', out_dim)
+        """ """
 
-        x = cross_attn(sampled_pc_embeddings, context = pc_embeddings, mask = None) + sampled_pc_embeddings
-        x = cross_ff(x) + x
 
-        mean = self.mean_fc(x)
-        logvar = self.logvar_fc(x)
+        """ Variational autoencoder encoding """
+        mean = self.mean_fc(x) # project to latent mean (B, N', latent_dim) - here N' is the number of latent vectors in latent set
+        logvar = self.logvar_fc(x) # project to latent log variance (B, N', latent_dim)
 
-        posterior = DiagonalGaussianDistribution(mean, logvar)
-        x = posterior.sample()
-        kl = posterior.kl()
+        posterior = DiagonalGaussianDistribution(mean, logvar) # wraps the parameters into a diagonal Gaussian distribution
+        x = posterior.sample() # sample from the distribution to get the latent vector set (B, N', latent_dim) - i.e. sample form the latent set of vectors
+        kl = posterior.kl() # get kl divergence of the posterior distribution
+        """ """
 
-        return kl, x
+        return kl, x # return: sampled latent vector set (B, N', latent_dim) from learned distribution and kl divergence of this distribution (from prior)
 
 
     def decode(self, x, queries):
 
-        x = self.proj(x)
+        x = self.proj(x) # map from latent set of N' vectors to core set of N' latent vectors (B, N', out_dim) -> (B, N', dim)
 
-        for self_attn, self_ff in self.layers:
+        """ Self-attention + feedforward on latent vectors """
+        for self_attn, self_ff in self.layers: # allow each of N' latent vectors to attend to each other
             x = self_attn(x) + x
             x = self_ff(x) + x
+        """ """
 
-        # cross attend from decoder queries to latents
-        queries_embeddings = self.point_embed(queries)
-        latents = self.decoder_cross_attn(queries_embeddings, context = x)
-
+        
+        """ Cross-attend from decoder queries to latents """
+        queries_embeddings = self.point_embed(queries) # embed queries (B, M, 3) to (B, M, out_dim) where M is the number of queries i.e. make the same point embeddings/tokens
+        latents = self.decoder_cross_attn(queries_embeddings, context = x) # each query point attends to (gets info from) the latent set of points (B, M, out_dim) -> (B, M, out_dim)
+        """ """
+        
         # optional decoder feedforward
         # if exists(self.decoder_ff):
         #     latents = latents + self.decoder_ff(latents)
         
-        return self.to_outputs(latents)
+        return self.to_outputs(latents) # for each query point embedding (B, M, out_dim) map it to SDF value (B, M, out_dim) -> (B, M, 1)
 
     def forward(self, pc, queries):
         kl, x = self.encode(pc)
--- trainer.py
+++ trainer.py
@@ -2,49 +2,56 @@ import torch
 import torch.nn.functional as F
 from tqdm import tqdm
 
-def train(model, train_loader, val_loader, optimizer, device, num_epochs, kl_weight=1e-3):
+
+def train(model, train_loader, val_loader, optimizer, device, num_epochs, kl_weight=1e-3, points_used=2048, num_query_points=512):
     for epoch in range(num_epochs):
-        train_one_epoch(model, train_loader, optimizer, device, kl_weight)
+        train_one_epoch(model, train_loader, optimizer, device, kl_weight, points_used, num_query_points)
         val_loss = evaluate(model, val_loader, device)
         print(f"Epoch {epoch+1}/{num_epochs} - Validation Loss: {val_loss:.6f}")
 
-def train_one_epoch(model, data_loader, optimizer, device, kl_weight, points_used = 2048, num_query_points=512):
+
+def train_one_epoch(model, data_loader, optimizer, device, kl_weight, points_used = 2048, num_query_points=2048):
     model.train()
 
     for batch in tqdm(data_loader):
-        B, N, D = batch.shape  # B=20, N=500, D=4
+        B, N, D = batch.shape  # B=10, N=50000, D=4
 
-        # Random indices: shape [B, points_used]
-        samples_idx = torch.randint(0, N, (B, points_used), device=batch.device)
-        query_idx = torch.randint(0, N, (B, num_query_points), device=batch.device)
+        """ Sample from point clouds in the batch """ # (each batch contains multiple point clouds)
+        ### input points ###
+        sample_pos = torch.zeros((B, points_used, 3), device=device) # shape [B, points_used, 3] - surface points sampled from each shape in the batch
 
-        # Use batch-wise indexing to gather the sampled points
-        sample_idx = torch.arange(B, device=batch.device).unsqueeze(1).expand(-1, points_used)
-        query_idx = torch.arange(B, device=batch.device).unsqueeze(1).expand(-1, num_query_points)
-
-        sampled_points = batch[sample_idx, samples_idx] # [B, points_used, 4]
-        query_points = batch[query_idx, query_idx] # [B, query_points, 4]
-
-        sample_pos = sampled_points[:, :, :3].to(device) # [B, points_used, 3]
+        for i in range(B):
+            shape = batch[i]  # [N, 4]
+            surface_pts = shape[shape[:, 3] == 0]  # sdf == 0 → surface points
+            # Random sample
+            sample_idx = torch.randperm(surface_pts.shape[0])[:points_used]
+            sample_pos[i] = surface_pts[sample_idx, :3]  # only x,y,z
+        
+        ### query points ###
+        querys_idx = torch.randint(0, N, (B, num_query_points), device=batch.device) # dim = (B, num_query_points)
+        query_idx = torch.arange(B, device=batch.device).unsqueeze(1).expand(-1, num_query_points) # [B, num_query_points]
+        # use advanced indexing to gather the sampled points
+        query_points = batch[query_idx, querys_idx] # [B, query_points, 4]
         query_pos = query_points[:, :, :3].to(device) # [B, points_used, 3]
-
         query_sdf = query_points[:, :, 3].to(device)   # shape [B, query_points]
+        """ """
+        # Note: effectively every batch we use a different subset of points from each shape in the batch
 
+        """ Perform step """
         optimizer.zero_grad()
-
         outputs = model(sample_pos, query_pos)
-
         predicted_sdfs = outputs['sdf']   # (B, N_query)
         kl_loss = outputs['kl']            # (B, )
 
-        sdf_loss = F.l1_loss(predicted_sdfs, query_sdf)
-
+        sdf_loss = F.mse_loss(predicted_sdfs, query_sdf)
         kl_loss = torch.mean(kl_loss)
 
         loss = sdf_loss + kl_weight * kl_loss
 
         loss.backward()
         optimizer.step()
+        """ """
+
 
 @torch.no_grad()
 def evaluate(model, data_loader, device, points_used = 2048, num_query_points=512):
@@ -56,26 +63,29 @@ def evaluate(model, data_loader, device, points_used = 2048, num_query_points=51
     for batch in data_loader:
         B, N, D = batch.shape  # B=20, N=500, D=4
 
-        # Random indices: shape [B, points_used]
-        samples_idx = torch.randint(0, N, (B, points_used), device=batch.device)
-        query_idx = torch.randint(0, N, (B, num_query_points), device=batch.device)
+        """ Sample from point clouds in the batch """ # (each batch contains multiple point clouds)
+        # Random indices: shape [B, points_used] - select indices of points for each shape in the batch
+        samples_idx = torch.randint(0, N, (B, points_used), device=batch.device) # dim = (B, points_id)
+        querys_idx = torch.randint(0, N, (B, num_query_points), device=batch.device) # dim = (B, num_query_points)
 
-        # Use batch-wise indexing to gather the sampled points
-        sample_idx = torch.arange(B, device=batch.device).unsqueeze(1).expand(-1, points_used)
-        query_idx = torch.arange(B, device=batch.device).unsqueeze(1).expand(-1, num_query_points)
+        # Use batch-wise indexing to build batch-wise index mask
+        sample_idx = torch.arange(B, device=batch.device).unsqueeze(1).expand(-1, points_used) # [B, points_used]
+        query_idx = torch.arange(B, device=batch.device).unsqueeze(1).expand(-1, num_query_points) # [B, num_query_points]
 
+        # use advanced indexing to gather the sampled points
         sampled_points = batch[sample_idx, samples_idx] # [B, points_used, 4]
-        query_points = batch[query_idx, query_idx] # [B, query_points, 4]
+        query_points = batch[query_idx, querys_idx] # [B, query_points, 4]
 
         sample_pos = sampled_points[:, :, :3].to(device) # [B, points_used, 3]
         query_pos = query_points[:, :, :3].to(device) # [B, points_used, 3]
 
         query_sdf = query_points[:, :, 3].to(device)   # shape [B, query_points]
+        """ """
 
         outputs = model(sample_pos, query_pos)
 
         predicted_sdfs = outputs['sdf']
-        loss = F.l1_loss(predicted_sdfs, query_sdf)
+        loss = F.mse_loss(predicted_sdfs, query_sdf)
 
         total_loss += loss.item()
         num_batches += 1
